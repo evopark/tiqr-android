7849ebc103a6944080de3aa162290c49abb8e2da
Marcus Ilgner <marcus.ilgner@evopark.de>
2015-03-24 17:16:36 +0100

    Add Titanium module skeleton

---
+++ src/main/java/de/evopark/titanium/qrscanner/android/ModuleInfo.java
@@ -0,0 +1,8 @@
+package de.evopark.titanium.qrscanner.android;
+
+/**
+ * A couple of static symbols to avoid repetition
+ */
+public class ModuleInfo {
+  public final static String MODULE_ID = "de.evopark.titanium.qrscanner.android";
+}
